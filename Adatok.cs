4f7a2caae4d3b6bf6ff700c812925febd9a9e35d
Szima Ákos <70635906+szimaakos@users.noreply.github.com>
2024-09-13 13:07:14 +0200

    Add files via upload

---
+++ Adatok.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _09_13_sandorral
+{
+    internal class Adatok
+    {
+        public string vezetek_nev;
+        public string kereszt_nev;
+        public int ID;
+        public int kor;
+
+        public Adatok(string s)
+        {
+            string[] darabok = s.Split(';');
+            this.vezetek_nev = darabok[0];
+            this.kereszt_nev = darabok[1];
+
+            string[] ints = darabok[2].Split("-");
+            this.ID = Convert.ToInt32(ints[0]+ints[2]);
+            this.kor = Convert.ToInt32(ints[1]);
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using _09_13_sandorral;
+using System.Collections.Concurrent;
+using System.IO;
+using System.Linq;
+
+List<Adatok> lista = new List<Adatok>();
+
+string faljnev = "people.csv";
+
+using (StreamReader sr = new StreamReader(faljnev))
+{
+    while (!sr.EndOfStream)
+    {
+        string sor = sr.ReadLine();
+        Adatok s = new Adatok(sor);
+        lista.Add(s);
+    }
+}
+
+Console.WriteLine($"A legidősebb személy neve: {lista
+    .OrderByDescending(x => x.kor)
+    .Select(x => $"{x.vezetek_nev} {x.kereszt_nev}")
+    .FirstOrDefault()}");
+
+Console.WriteLine($"A legfiatalabb személy neve: {lista
+    .OrderBy(x => x.kor)
+    .Select(x => $"{x.vezetek_nev} {x.kereszt_nev}")
+    .FirstOrDefault()}");
+
+Console.WriteLine($"Átlag életkor: {lista.Average(x=>x.kor)}");
